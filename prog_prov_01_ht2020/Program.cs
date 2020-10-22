3a83947ff46d90378b9032af72a480268bb50f67
BlommaWalla <lutz.hahlbeckmail.com>
2020-10-22 15:05:27 +0200

    Add project files.

---
+++ prog_prov_01_ht2020/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace prog_prov_01_ht2020 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
